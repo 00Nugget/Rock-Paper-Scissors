444aae17c28bbb8007e246be5b78b7df224c87de
0Nugget <141096345+00Nugget@users.noreply.github.com>
2023-10-19 20:26:16 -0600

    Add playRound function

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function getComputerChoice() {
     return (choices[(Math.floor(Math.random() * choices.length))]);
 
 }
+function playRound(playerSelection, computerSelection){
+    if (computerSelection === playerSelection) {
+        return print( "It's a tie!");
+    } else if (computerSelection = "scissors") {
+        return print ("You win! Rock beats Scissors!")
+    } else {
+        return print( "You lose! Paper beats Rock!")
+    }
+    }
+        
