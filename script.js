fef07bb3b8963d24720bad97a6de4d097a89d74e
0Nugget <141096345+00Nugget@users.noreply.github.com>
2023-10-24 15:10:36 -0600

    Replace playRound function with showMessage
    playRound message was changed to showMessage because it
    better describes what is happening in the code. The
    playRound function should have the code for computer
    and player choosing an option. The message can either be
    included later or remain as a separate function as showMessage

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function playRound(playerSelection, computerSelection){
     if (computerSelection === playerSelection) {
          console.log( `Computer chose: "${computerSelection}." You chose: "${playerSelection}." It's a tie!`);
     } else if (computerSelection === "Scissors") {
-        console.log(`Computer chose: "${computerSelection}" and you chose: "${playerSelection}". You win! Rock beats Scissors!`)
+        console.log(`Computer chose: "${computerSelection}" and you chose: "${playerSelection}". You win! Rock beats Scissors!`) 
     } else {
         console.log( "You lose! Paper beats Rock!")
     }
@@ -24,8 +24,11 @@ function playRound(playerSelection, computerSelection){
 const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 
-for (let i = 0; i < 5; i++) {
-    console.log(playRound(playerSelection,computerSelection));
+function game() {
+    return playRound(playerSelection,computerSelection);
 }
+//for (let i = 0; i < 5; i++) {
+    console.log(game());
+//}
 
 
