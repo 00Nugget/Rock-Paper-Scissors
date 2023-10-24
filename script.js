e95c9cfb0f45e79faf29f28184b8a830dcb1a566
0Nugget <141096345+00Nugget@users.noreply.github.com>
2023-10-24 15:46:16 -0600

    Add function getPlayerChoice

--- script.js
+++ script.js
@@ -10,8 +10,17 @@ let choices = [
 function getComputerChoice() {
     return choices[Math.floor(Math.random() *choices.length )];
 }
+function getPlayerChoice(playerSelection,computerSelection) {
+    let playerSelection = prompt("Rock? Paper? Scissors?","Rock");
+    if (playerSelection != null){
+        playerSelection + " selected. Good luck!"
+    } else {
+        alert ("Invalid input: Please choose either Rock, Paper, or Scissors!")
+    }
+    }
 
-function playRound(playerSelection, computerSelection){
+
+function showMessage(playerSelection, computerSelection){
     if (computerSelection === playerSelection) {
          console.log( `Computer chose: "${computerSelection}." You chose: "${playerSelection}." It's a tie!`);
     } else if (computerSelection === "Scissors") {
@@ -21,14 +30,14 @@ function playRound(playerSelection, computerSelection){
     }
     }
         
-const playerSelection = "Rock";
+const playerSelection = "";
 const computerSelection = getComputerChoice();
 
 function game() {
-    return playRound(playerSelection,computerSelection);
+    return showMessage(playerSelection,computerSelection);
 }
 //for (let i = 0; i < 5; i++) {
     console.log(game());
 //}
-
+// computerSelection should be chosen at random each time the function playRound is called
 
