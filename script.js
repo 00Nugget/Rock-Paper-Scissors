4b91cc725ec491486c1b37c0f7a50222b5749c77
0Nugget <141096345+00Nugget@users.noreply.github.com>
2023-10-24 17:29:42 -0600

    Removed playRound function
    Removed playRound function for debugging. Added text to
    getPlayerChoice and getComputerChoice for usability.

--- script.js
+++ script.js
@@ -7,20 +7,45 @@ let choices = [
 //function getComputerChoice() {
    // return (choices[(Math.floor(Math.random() * choices.length))]);
 //}
+
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() *choices.length )];
+    return (choices[Math.floor(Math.random() *choices.length )] + " chosen by computer.");
 }
-function getPlayerChoice(playerSelection,computerSelection) {
-    let playerSelection = prompt("Rock? Paper? Scissors?","Rock");
+/* function getPlayerChoice() {
     if (playerSelection != null){
-        playerSelection + " selected. Good luck!"
+       return playerSelection 
     } else {
         alert ("Invalid input: Please choose either Rock, Paper, or Scissors!")
     }
-    }
-
-
-function showMessage(playerSelection, computerSelection){
+    //return playerSelection;
+    } */
+/* 
+function playRound(playerSelection,computerSelection){
+    let playerSelection = prompt("Rock? Paper? Scissors?","Rock");
+    if (playerSelection != null) {
+      if (computerSelection === playerSelection) {
+        console.log( `Computer chose: "${computerSelection}." You chose: "${playerSelection}." It's a tie!`);
+      }else if (computerSelection === "Scissors", 
+        playerSelection === "Rock") { 
+        console.log(`Computer chose: "${computerSelection}" and you chose: "${playerSelection}". You win! Rock beats Scissors!`) 
+      } else if (computerSelection === "Scissors"   playerSelection === "Paper") {
+       console.log( "You lose! Scissors beats Paper!")
+         } else if (computerSelection === "Rock", playerSelection === "Paper") {
+        console.log("You win! Paper beats Rock")
+         } else if (computerSelection === "Rock", playerSelection === "Scissors") {
+            console.log ("You lose! Rock beats Scissors!")
+         } else if (computerSelection === "Paper", playerSelection === "Rock") {
+            console.log("You lose! Paper beats Rock!") 
+        } else if (computerSelection === "Paper", playerSelection === "Scissors") {
+         console.log("You win! Scissors beats Paper!")
+         } else {
+            alert ("Error")
+            } 
+   return playerSelection  
+    } else {
+        alert ("Invalid input: Please choose either Rock, Paper, or Scissors!");
+       }  */
+/* function showMessage(playerSelection, computerSelection){
     if (computerSelection === playerSelection) {
          console.log( `Computer chose: "${computerSelection}." You chose: "${playerSelection}." It's a tie!`);
     } else if (computerSelection === "Scissors") {
@@ -28,16 +53,20 @@ function showMessage(playerSelection, computerSelection){
     } else {
         console.log( "You lose! Paper beats Rock!")
     }
-    }
-        
-const playerSelection = "";
+    } */
+
+function getPlayerChoice() {
+    return (prompt ("Rock, Paper,Scissors","Rock") + " chosen by player.") 
+}
+
+
+const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 
-function game() {
-    return showMessage(playerSelection,computerSelection);
-}
+console.log(playerSelection,computerSelection);
+
+//}
 //for (let i = 0; i < 5; i++) {
-    console.log(game());
+ //   console.log(game());
 //}
 // computerSelection should be chosen at random each time the function playRound is called
-
