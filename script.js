d96527ddf425458a8af96539e2b52703494e11d6
0Nugget <141096345+00Nugget@users.noreply.github.com>
2023-10-30 19:03:29 -0600

    Add scoring to playRound function

--- script.js
+++ script.js
@@ -13,39 +13,35 @@ let playerScore = 0;
 let noContestScore = 0;
 
 function playRound(computerSelection, playerSelection) {  //playRound function goes through all the possible outcomes 
-  
     if (computerSelection === playerSelection) {
-        return console.log(playerSelection, computerSelection), noContest;
+        return console.log(playerSelection, computerSelection), playerScore+0, computerScore+0;
       } 
       else if (computerSelection === "Scissors") {
         if (playerSelection === "Rock") {
-          return console.log("P: "+ playerSelection, "C: "+ computerSelection), playerRoundVictory;
+          return console.log("P: "+ playerSelection, "C: "+ computerSelection), playerScore++, computerScore+0;
             } 
           else if (playerSelection === "Paper") {
-            return console.log("P: "+ playerSelection, "C: "+ computerSelection),computerRoundVictory;
+            return console.log("P: "+ playerSelection, "C: "+ computerSelection),computerScore++,playerScore+0;
           }
         }
       else if (computerSelection === "Rock") {
           if (playerSelection === "Paper") {
-            return console.log("P: "+ playerSelection, "C: "+ computerSelection),playerRoundVictory;
+            return console.log("P: "+ playerSelection, "C: "+ computerSelection),playerScore++, computerScore+0;
             }
         else if (playerSelection === "Scissors") {
-          return console.log("P: "+ playerSelection, "C: "+ computerSelection),computerRoundVictory;
+          return console.log("P: "+ playerSelection, "C: "+ computerSelection),computerScore++,playerScore+0;
         }
         }
       else if (computerSelection ==="Paper") {
           if (playerSelection === "Scissors") {
-              return console.log("P: "+ playerSelection, "C: "+ computerSelection),playerRoundVictory;
+              return console.log("P: "+ playerSelection, "C: "+ computerSelection),playerScore++,computerScore+0;
           }
           else if (playerSelection === "Rock"){
-            return console.log("P: "+ playerSelection, "C: "+ computerSelection),computerRoundVictory;
+            return console.log("P: "+ playerSelection, "C: "+ computerSelection),computerScore++,playerScore+0;
         }
       }
     }
 
-let playerRoundVictory
-let computerRoundVictory
-let noContest
 
 for (let i = 0; i < 5; i++) { // Plays up to 5 rounds of the game
 let playerSelection = prompt("Rock, Paper, Scissors?", "Rock"); //defining player choice here with a prompt each time asking for RPS (Rock paper scissors)
@@ -53,14 +49,9 @@ const computerSelection = getComputerChoice();
 playRound(computerSelection,playerSelection);
 console.log("Your Score: "+ playerScore);
 console.log("Computer Score: " + computerScore);
-if (playRound === playerRoundVictory) {
-  playerScore++;
-} else if (playRound === computerRoundVictory) {
-      computerScore++;
-} else {
-  noContestScore++;
+
 }
-}
+
 
 
 /* function game(playerScore, computerScore) {
