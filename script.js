f50ecf74b99587832829f65aceb7f8e1a013f2cb
0Nugget <141096345+00Nugget@users.noreply.github.com>
2023-11-01 12:35:13 -0600

    Make prompt case insensitive
    Changed string to lowercase and add .toLowerCase function for both
    computer and player choice functions

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let choices = [ //choices for random computer choice
-    "Rock" , 
-    "Paper", 
-    "Scissors" 
-];
+const choices = [ //choices for random computer choice
+    "rock" , 
+    "paper", 
+    "scissors" 
+  ];
+const lowerCaseArray = choices.map(item => item.toLowerCase());
 
 
 function getComputerChoice() {
-    return (choices[Math.floor(Math.random() *choices.length )]) //+ " chosen by computer.");
+    return (choices[Math.floor(Math.random() *choices.length )]); //+ " chosen by computer.");
 }
 let computerScore = 0;
 let playerScore = 0;
@@ -14,37 +15,37 @@ let playerScore = 0;
 
 function playRound(computerSelection, playerSelection) {  //playRound function goes through all the possible outcomes 
     if (computerSelection === playerSelection) {
-        return console.log("Player chose: " +playerSelection,"Computer chose: " + computerSelection +" .Draw!"), playerScore+0, computerScore+0;
+        return console.log("Player chose: " + playerSelection,"Computer chose: " + computerSelection +" .Draw!"), playerScore+0, computerScore+0;
       } 
-      else if (computerSelection === "Scissors") {
-        if (playerSelection === "Rock") {
+      else if (computerSelection === "scissors") {
+        if (playerSelection === "rock") {
           return console.log("Player chose: "+ playerSelection, "Computer chose: "+ computerSelection), playerScore++, computerScore+0;
             } 
-          else if (playerSelection === "Paper") {
+          else if (playerSelection === "paper") {
             return console.log("Player chose: "+ playerSelection, "Computer chose: "+ computerSelection),computerScore++,playerScore+0;
           }
         }
-      else if (computerSelection === "Rock") {
-          if (playerSelection === "Paper") {
+      else if (computerSelection === "rock") {
+          if (playerSelection === "paper") {
             return console.log("Player chose: "+ playerSelection, "Computer chose: "+ computerSelection),playerScore++, computerScore+0;
             }
-        else if (playerSelection === "Scissors") {
+        else if (playerSelection === "scissors") {
           return console.log("Player chose: "+ playerSelection, "Computer chose: "+ computerSelection),computerScore++,playerScore+0;
         }
         }
-      else if (computerSelection ==="Paper") {
-          if (playerSelection === "Scissors") {
+      else if (computerSelection ==="paper") {
+          if (playerSelection === "scissors") {
               return console.log("Player chose: "+ playerSelection, "Computer chose: "+ computerSelection),playerScore++,computerScore+0;
           }
-          else if (playerSelection === "Rock"){
+          else if (playerSelection === "rock"){
             return console.log("Player chose: "+ playerSelection, "Computer chose: "+ computerSelection),computerScore++,playerScore+0;
         }
       }
-    }
+    }//defining player choice here with a prompt each time asking for RPS (Rock paper scissors)
 
 
 for (let i = 0; i < 1000; i++) { // Plays up to 5 rounds of the game
-let playerSelection = prompt("Rock, Paper, Scissors?", "Rock"); //defining player choice here with a prompt each time asking for RPS (Rock paper scissors)
+let playerSelection = prompt("Rock, Paper, Scissors?", "Rock").toLowerCase();
 const computerSelection = getComputerChoice();
 playRound(computerSelection,playerSelection);
 console.log("Your Score: "+ playerScore);
